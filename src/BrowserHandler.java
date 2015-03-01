f261f329077f09c528798082d55f13aa8751acdf
jjp75 <jjp75@pitt.edu>
2015-03-01 13:18:14 -0500

    Updated base class to automatically load https://github.com/

--- src/BrowserHandler.java
+++ src/BrowserHandler.java
@@ -2,12 +2,13 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class BrowserHandler {
+public class BrowserHandler {	
 	private static FirefoxDriver firefoxDriver;
 	
 	@BeforeClass
 	public static void setUp() {
 		firefoxDriver = new FirefoxDriver();
+		firefoxDriver.get("https://github.com/");
 	}
 	
 	@AfterClass
