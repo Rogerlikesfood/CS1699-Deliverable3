7e66918e0015ad642bb6de06690704c43b7a94e1
jjp75 <jjp75@pitt.edu>
2015-03-01 16:21:13 -0500

    Renamed SignIn file to SignInTests

---
+++ src/SignInTests.java
@@ -0,0 +1,44 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SignInTests extends InitialSetup {
+	@Before
+	@Override
+	public void setUp() {
+		super.setUp();
+	}
+	
+	@Test
+	public void signInWithoutHavingAGitHubAccount() {
+		
+	}
+	
+	@Test
+	public void signInWithIncorrectUsernameAndIncorrectPassword() {
+		
+	}
+	
+	@Test
+	public void signInWithIncorrectUsernameAndCorrectPassword() {
+		
+	}
+	
+
+	@Test
+	public void signInWithCorrectUsernameAndIncorrectPassword() {
+		
+	}
+	
+
+	@Test
+	public void signInWithCorrectUsernameAndCorrectPassword() {
+		
+	}
+	
+	@After
+	@Override
+	public void tearDown() {
+		super.tearDown();
+	}
+}
